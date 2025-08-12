40899aa90f27f76b056c41e218bb97cbe522c0fa
AshamedConsequence <60143430+AshamedConsequence@users.noreply.github.com>
2025-08-12 09:03:30 +0900

    Update config.el

--- config.el
+++ config.el
@@ -5,40 +5,60 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Maxime Morize"
-			user-mail-address "maxime.morize@outlook.com")
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Inconsolata" :size 16))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+(setq doom-font (font-spec :family "Maple Mono Normal NL NF CN" :size 24 :weight 'semi-light))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
 
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
 
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
 
-
-;; Here are some additional functions/macros that could help you configure Doom:
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
+;; - `use-package!' for configuring packages
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -46,64 +66,82 @@
 ;; - `map!' for binding new keys
 ;;
 ;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+;;; ~/.doom.d/config.el
+
+;; 1. Define a function to hold your new banner.
+;;    The backslashes (\) in the original art must be escaped (\\).
+;; (defun my-custom-rip-banner ()
+;;   "A custom banner to display on the Doom dashboard."
+;;   (insert
+;;    (concat
+;;     "\n"
+(defun my-weebery-is-always-greater ()
+  (let* ((banner '("  .-''-.                      .--.  .-. .--------.                      .-''-."
+                   " /      `'''--..              ||  \\/  ||'=====. ||              ..--'''`      \\"
+                   "|               '.            || .  . ||  .---' ||            .'               |"
+                   "|           ..''` '---.       || |\\/|_||  '===. ||       .---' `''..           |"
+                   "|        .``           ''''\\  ||_|\\/|.'       |_||  /''''           ``.        |"
+                   "/'..   /`              /    /'|_.'            '._|'\\    \\              `\\   ..'\\"
+                   "|   `:'         ___..  \\   /  /                  \\  \\   /  ..___         ':`   |"
+                   "'____'__...---'':::::\\  '-' ./                    \\. '-'  \\:::::''---...__'____'"
+                   " \\::/\\ \\:::::::::::':  ___/                        \\___  :':::::::::::/ /\\::/"
+                   "   \\'| \\ '-:::--'`  .' /              Rest              \\ '.  `'--:::-' / |'/"
+                   "   /'|  \\    ....''`__/                in                \\__`''....    /  |'\\"
+                   "   \\ |   .   |  .-'`   .------. .------..------. .-. .--.   `'-.  |   .   | /"
+                   "    \\/::.'   |  |      ||  _   V   _   ||   _   V  \\/  ||      |  |   '.::\\/"
+                   "    \\':'_.---|  |      || | |  |  | |  ||  | |  | .  . ||      |  |---._':'/"
+                   "     \\\\      |  |      || | |  |   '|  ||  |'   | |\\/| ||      |  |      //"
+                   "     \\\\      |  |      || |' .'|'.   .'||'.   .'|'|\\/| ||      |  |      //"
+                   "      \\\\      | |      ||  .'.' '.'.'.'  '.'.'.' '|  | ||      | |      //"
+                   "      \\\\      | |      ||.'.'     '.'      '.'       |.||      | |      //"
+                   "       \\\\     |'       |_.'                          '._|       '|     //"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'my-weebery-is-always-greater)
+
+(after! dape
+        (add-to-list 'dape-configs
+                `(debugpy modes (python-mode python-ts-mode)
+                        port 5678
+                        command "python"
+                        command-args ("-m" "debugpy.adapter" "--host" "0.0.0.0" "--port" :autoport)
+                        :cwd dape-cwd
+                        :program dape-buffer-default
+                        :args []
+                        :justMyCode nil
+                        :console "integratedTerminal"
+                        :showReturnValue t
+                        :stopOnEntry nil)))
+
+(after! magit
+  (setq magit-diff-refine-hunk 'all))
+(setq dired-listing-switches "-aBhl  --group-directories-first")
+
+;; Set background opacity to 85% for the current frame
 ;;
-(def-package! org-super-agenda
-	:after org-agenda
-	:init
-	(setq org-super-agenda-groups '((:name "Today"
-																				 :time-grid t
-																				 :scheduled today)
-																	(:name "Due today"
-																				 :deadline today)
-																	(:name "Important"
-																					:priority "A")
-																	(:name "Overdue"
-																					:deadline past)
-																	(:name "Due soon"
-																					:deadline future)
-																	(:name "Big Outcomes"
-																				 :tag "bo")))
-	:config
-	(org-super-agenda-mode))
+(set-frame-parameter nil 'alpha-background 85)
 
-(add-to-list 'load-path "~/undo-tree/")
-(global-undo-tree-mode)
+;; Set background opacity for all future frames
+(add-to-list 'default-frame-alist '(alpha-background . 85))
+(use-package! nyan-mode
+  :config
+  (nyan-mode)
+  (nyan-start-animation))
 
-(map! :leader
-			:prefix "o"
-			:desc "Undo Tree" "u" #'undo-tree-visualize)
-
-(map! :leader
-			:prefix "o"
-			:desc "Weather" "w" #'wttrin)
-(setq wttrin-default-cities '("Katy" "Paris" "Houston"))
-
-(require 'origami)
-(global-origami-mode)
-
-(setq-default indent-tabs-mode t)
-
-(latex-preview-pane-enable)
-(require 'web-beautify) ;; Not necessary if using ELPA package
-(eval-after-load 'js2-mode
-	'(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-;; Or if you're using 'js-mode' (a.k.a 'javascript-mode')
-(eval-after-load 'js
-	'(define-key js-mode-map (kbd "C-c b") 'web-beautify-js))
-
-(eval-after-load 'json-mode
-	'(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
-
-(eval-after-load 'sgml-mode
-	'(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
-
-(eval-after-load 'web-mode
-	'(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
-
-(eval-after-load 'css-mode
-	'(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
+(after! nyan-mode
+  (setq nyan-wavy-trail t))
