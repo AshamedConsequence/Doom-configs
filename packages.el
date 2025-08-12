2ca344c02f429cc3be7e48ded042848b412231f9
AshamedConsequence <60143430+AshamedConsequence@users.noreply.github.com>
2025-08-12 09:05:15 +0900

    Update packages.el

--- packages.el
+++ packages.el
@@ -50,7 +50,6 @@
 ;; our package manager can't deal with; see raxod502/straight.el#279)
 ;(package! builtin-package :recipe (:branch "develop"))
 (package! org-super-agenda)
-(package! origami)
 (package! wttrin)
 (package! pdf-tools)
 (package! web-beautify)
